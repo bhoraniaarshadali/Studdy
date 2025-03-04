4d73176031aca41a50d5f6ffa2c97c49da4afc12
Arshad ali bhorania <bhoraniyaarshadali431@gmail.com>
2025-03-05 01:59:43 +0530

    Add SplashActivity

---
+++ app/src/main/java/com/example/studdy/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.studdy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
